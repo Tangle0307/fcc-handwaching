4ba14b3524e3bfd495d868add27afd357aaa77e1
Tangle0307 <62170193+Tangle0307@users.noreply.github.com>
2021-02-02 11:36:24 +0900

    commiting html

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,27 @@
-<h1>Test</h1>
+<script>
+    import Timer from "./Timer.svelte";
+    import HowTo from "./HowTo.svelte";
+</script>
+
+<h1>Handwashing App</h1>
+
+<Timer />
+
+<HowTo />
+
+<h3>
+    <a href="https://www.unwater.org/app/uploads/2020/03/handwashing.png"
+        >Picture Source</a
+    >
+
+    <a href="https://freesound.org/people/metrostock99/sounds/345086/"
+        >Sound Source</a
+    >
+</h3>
+
+<style>
+    h1,
+    h3 {
+        text-align: center;
+    }
+</style>
