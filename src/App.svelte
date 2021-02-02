20d5580a066356e7896daaa99d89f1f11415afab
Tangle0307 <62170193+Tangle0307@users.noreply.github.com>
2021-02-02 16:26:17 +0900

    got sound to work

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
 <script>
     import Timer from "./Timer.svelte";
     import HowTo from "./HowTo.svelte";
+    let audio;
+
+    function timerEnds(e) {
+        audio.play();
+    }
 </script>
 
 <h1>Handwashing App</h1>
 
-<Timer />
+<!-- svelte-ignore missing-declaration -->
+<Timer on:end={timerEnds} />
 
 <HowTo />
 
@@ -13,11 +19,12 @@
     <a href="https://www.unwater.org/app/uploads/2020/03/handwashing.png"
         >Picture Source</a
     >
-
-    <a href="https://freesound.org/people/metrostock99/sounds/345086/"
-        >Sound Source</a
-    >
 </h3>
+
+<!-- svelte-ignore a11y-media-has-caption -->
+<audio bind:this={audio}>
+    <source src="audio_sample.mp3" />
+</audio>
 
 <style>
     h1,
