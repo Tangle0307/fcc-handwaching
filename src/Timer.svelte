22c24e5373a4542347a0ec540893db6a02322598
Tangle0307 <62170193+Tangle0307@users.noreply.github.com>
2021-02-02 11:54:42 +0900

    got the timer code done

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,13 +1,37 @@
 <script>
     import ProgressBar from "./ProgressBar.svelte";
+
+    const totalSeconds = 20;
+    let secondsLeft = totalSeconds;
+    let isRunning = false;
+    $: progress = ((totalSeconds - secondsLeft) / totalSeconds) * 100;
+
+    function startTimer() {
+        isRunning = true;
+        let timer = setInterval(() => {
+            secondsLeft -= 1;
+            if (secondsLeft == 0) {
+                clearInterval(timer);
+                isRunning = false;
+                secondsLeft = totalSeconds;
+            }
+        }, 1000);
+    }
 </script>
 
 <div bp="grid">
-    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:</h2>
+    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left: {secondsLeft}</h2>
 </div>
-<ProgressBar />
+<ProgressBar {progress} />
 
-<button class="start">Start</button>
+<div bp="grid">
+    <button
+        disabled={isRunning}
+        bp="offset-5@md 4@md 12@sm"
+        class="start"
+        on:click={startTimer}>Start</button
+    >
+</div>
 
 <style>
     h2 {
@@ -17,5 +41,10 @@
         background-color: rgb(154, 73, 73);
         width: 100%;
         margin: 10px 0;
+    }
+
+    .start[disabled] {
+        background-color: rgb(194, 194, 194);
+        cursor: not-allowed;
     }
 </style>
